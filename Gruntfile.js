28f7e356759fb4eb1c41541ad0a7450e0ebbe680
Dave McNally <hello@dave.mn>
2019-06-05 17:06:46 +0100

    Merge to Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  const sass = require('node-sass');
+  const imageminGifsicle = require('imagemin-gifsicle');
+  const imageminJpegtran = require('imagemin-jpegtran');
+  const imageminOptipng = require('imagemin-optipng');
+  const mozjpeg = require('imagemin-mozjpeg');
+  grunt.initConfig({
+
+  });
+
+  grunt.registerTask();
+
+};
