be48c124d97525992532df85e749321e65879d8b
Dave McNally <hello@dave.mn>
2019-06-05 21:26:18 +0100

    Cleanup default _S files.

--- footer.php
+++ footer.php
@@ -11,23 +11,23 @@
 
 ?>
 
-	</div><!-- #content -->
+  </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hectopress' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hectopress' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hectopress' ), 'hectopress', '<a href="https://hectogon.studio">Hectogon</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer">
+    <div class="site-info">
+      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hectopress' ) ); ?>">
+        <?php
+        /* translators: %s: CMS name, i.e. WordPress. */
+        printf( esc_html__( 'Proudly powered by %s', 'hectopress' ), 'WordPress' );
+        ?>
+      </a>
+      <span class="sep"> | </span>
+        <?php
+        /* translators: 1: Theme name, 2: Theme author. */
+        printf( esc_html__( 'Theme: %1$s by %2$s.', 'hectopress' ), 'hectopress', '<a href="https://hectogon.studio">Hectogon</a>' );
+        ?>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
